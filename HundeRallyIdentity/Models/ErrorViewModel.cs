bf361088d6199eeb0e347a01778c4c89355d1ef3
IdaLuna <Idalunaditte@gmail.com>
2024-04-26 12:37:01 +0200

    Add project files.

---
+++ HundeRallyIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HundeRallyIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HundeRallyIdentity/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HundeRallyIdentity.Models
+{
+    public class User : IdentityUser
+    {
+        public string UserName { get; set; }
+    }
+}
