0962ac5ee2db15f6e873d3611e1b0c4deacf3523
IdaLuna <Idalunaditte@gmail.com>
2024-05-01 16:31:21 +0200

    Added Roles

--- HundeRallyIdentity/Models/User.cs
+++ HundeRallyIdentity/Models/User.cs
@@ -4,6 +4,6 @@ namespace HundeRallyIdentity.Models
 {
     public class User : IdentityUser
     {
-        public string UserName { get; set; }
+       // public string UserName { get; set; }
     }
 }
--- HundeRallyIdentity/Program.cs
+++ HundeRallyIdentity/Program.cs
@@ -10,9 +10,42 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+
 
 var app = builder.Build();
 
@@ -40,4 +73,20 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
-app.Run();
+using (var scope = app.Services.CreateScope()) //To access the services we have configured above //add roles by seeding
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] { "Admin", "Instructor","Judge", "Handler"};
+
+    foreach (var role in roles) 
+    {
+        
+        if(!await roleManager.RoleExistsAsync(role)) //Check if the role already exists
+            await roleManager.CreateAsync(new IdentityRole(role)); // if not, create it
+
+    }
+
+}
+
+    app.Run();
