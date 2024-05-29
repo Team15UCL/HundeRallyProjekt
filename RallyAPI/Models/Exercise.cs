b44e4e65bd7c36210e52e35ae2d09dc8d29da8e8
Aske <askelysgaard@gmail.com>
2024-05-29 21:44:06 +0200

    localdb implementeret

--- RallyAPI/Models/Exercise.cs
+++ RallyAPI/Models/Exercise.cs
@@ -4,8 +4,8 @@ namespace RallyAPI.Models;
 
 public class Exercise
 {
-    public ObjectId Id { get; set; }
-    public string Name { get; set; }
+	public ObjectId Id { get; set; }
+	public string Name { get; set; }
     public string Class { get; set; }
     public string Url { get; set; }
 }
--- RallyAPI/Program.cs
+++ RallyAPI/Program.cs
@@ -1,4 +1,3 @@
-
 using RallyAPI.Data;
 using RallyAPI.Models.Services;
 
